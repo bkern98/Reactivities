f1f2e8803733d731a480268da1c4d7b4e924f8a1
bkern98 <bkern288@gmail.com>
2023-03-23 12:53:12 -0400

    activity details for selected activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,9 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined)
 
   useEffect(() => {
     axios
@@ -17,11 +20,24 @@ function App() {
       })
   }, [])
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(a => a.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined)
+  }
+
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+        />
       </Container>
     </>
   )

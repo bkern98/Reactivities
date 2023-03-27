c7c9737d2a07d9dd456eec0d7ce4645954948642
bkern98 <bkern288@gmail.com>
2023-03-27 19:00:27 -0400

    set up client side routing

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,19 +1,24 @@
 import { observer } from 'mobx-react-lite'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { Button, Form, Segment } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store'
+import { v4 as uuid } from 'uuid'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore()
   const {
-    selectedActivity,
-    closeForm,
     loading,
     createActivity,
-    updateActivity
+    updateActivity,
+    loadActivity,
+    loadingInitial
   } = activityStore
+  const { id } = useParams()
+  const navigate = useNavigate()
 
-  const initialState = selectedActivity ?? {
+  const [activity, setActivity] = useState({
     id: '',
     title: '',
     category: '',
@@ -21,12 +26,23 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: ''
-  }
+  })
 
-  const [activity, setActivity] = useState(initialState)
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+  }, [id, loadActivity])
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity)
+    if (!activity.id) {
+      activity.id = uuid()
+      createActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      )
+    } else {
+      updateActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      )
+    }
   }
 
   function handleInputChange(
@@ -35,6 +51,8 @@ export default observer(function ActivityForm() {
     const { name, value } = event.target
     setActivity({ ...activity, [name]: value })
   }
+
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
   return (
     <Segment clearing>
@@ -84,7 +102,8 @@ export default observer(function ActivityForm() {
           content='Submit'
         />
         <Button
-          onClick={closeForm}
+          as={Link}
+          to='/activities'
           floated='right'
           type='button'
           content='Cancel'

f2f2176c557d9f8de8ec1bc923e1f6cdbaca000e
bkern98 <bkern288@gmail.com>
2023-04-06 09:00:57 -0400

    Add error handling to endpoints

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -5,24 +6,24 @@ using Persistence;
 
 namespace Application.Activities
 {
-  public class List
-  {
-    public class Query : IRequest<List<Activity>> { }
-
-    public class Handler : IRequestHandler<Query, List<Activity>>
+    public class List
     {
-      private readonly DataContext _context;
+        public class Query : IRequest<Result<List<Activity>>> { }
 
-      public Handler(DataContext context)
-      {
-        _context = context;
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        {
+            private readonly DataContext _context;
 
-      }
+            public Handler(DataContext context)
+            {
+                _context = context;
 
-      public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-      {
-        return await _context.Activities.ToListAsync();
-      }
+            }
+
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+            }
+        }
     }
-  }
 }
